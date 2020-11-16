f7a08f45e6b769ff375a4d6228304651c1dcad21
Taras Romilovych <tarasromil@gmail.com>
2020-11-17 00:51:57 +0200

    init

---
+++ index.ts
@@ -0,0 +1,65 @@
+import { EventEmitter } from 'events';
+
+namespace Core {
+  export interface IConnectionOptions {
+    host: string;
+    port?: number;
+  }
+
+  export interface IConnection extends EventEmitter {
+    host: string;
+    port: number;
+    connect(): Promise<void>;
+    send(...args: any[]): Promise<any>;
+    send(buffer: Buffer): Promise<any>;
+    disconnect(): void;
+  }
+
+  export class Connection extends EventEmitter implements IConnection {
+    public static EventType = {
+      Connect: 'connect',
+      Error: 'error',
+      Message: 'message',
+      Disconnect: 'disconnect',
+    };
+    protected static SocketEventType = {
+      Connect: 'connect',
+      Close: 'close',
+      Error: 'error',
+      Readable: 'readable',
+    };
+    // @ts-ignore
+    protected _host: string;
+    protected _port: number = 0;
+
+    constructor(options: IConnectionOptions) {
+      super();
+      this._host = options.host;
+      if (options.port !== undefined) {
+        this._port = options.port;
+      }
+    }
+
+    public get host() {
+      return this._host;
+    }
+
+    public get port() {
+      return this._port;
+    }
+
+    public async connect() {
+      throw new Error('connect: No implementation');
+    }
+
+    public async send(...args: any[]) {
+      throw new Error('connect: No implementation');
+    }
+
+    public disconnect() {
+      throw new Error('disconnect: No implementation');
+    }
+  }
+}
+
+export default Core;
