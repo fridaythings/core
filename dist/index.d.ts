9042c98f0ff97d889fcccbd316fd8bf2cbda090a
Taras Romilovych <tarasromil@gmail.com>
2020-11-18 23:14:25 +0200

    include dist

---
+++ dist/index.d.ts
@@ -0,0 +1,39 @@
+/// <reference types="node" />
+import { EventEmitter } from 'events';
+declare namespace Core {
+    interface IConnectionOptions {
+        host: string;
+        port?: number;
+    }
+    interface IConnection extends EventEmitter {
+        host: string;
+        port: number;
+        connect(): Promise<void>;
+        send(...args: any[]): Promise<any>;
+        send(buffer: Buffer): Promise<any>;
+        disconnect(): void;
+    }
+    class Connection extends EventEmitter implements IConnection {
+        static EventType: {
+            Connect: string;
+            Error: string;
+            Message: string;
+            Disconnect: string;
+        };
+        protected static SocketEventType: {
+            Connect: string;
+            Close: string;
+            Error: string;
+            Readable: string;
+        };
+        protected _host: string;
+        protected _port: number;
+        constructor(options: IConnectionOptions);
+        get host(): string;
+        get port(): number;
+        connect(): Promise<void>;
+        send(...args: any[]): Promise<void>;
+        disconnect(): void;
+    }
+}
+export default Core;
---
+++ dist/index.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const events_1 = require("events");
+var Core;
+(function (Core) {
+    class Connection extends events_1.EventEmitter {
+        constructor(options) {
+            super();
+            this._port = 0;
+            this._host = options.host;
+            if (options.port !== undefined) {
+                this._port = options.port;
+            }
+        }
+        get host() {
+            return this._host;
+        }
+        get port() {
+            return this._port;
+        }
+        async connect() {
+            throw new Error('connect: No implementation');
+        }
+        async send(...args) {
+            throw new Error('connect: No implementation');
+        }
+        disconnect() {
+            throw new Error('disconnect: No implementation');
+        }
+    }
+    Connection.EventType = {
+        Connect: 'connect',
+        Error: 'error',
+        Message: 'message',
+        Disconnect: 'disconnect',
+    };
+    Connection.SocketEventType = {
+        Connect: 'connect',
+        Close: 'close',
+        Error: 'error',
+        Readable: 'readable',
+    };
+    Core.Connection = Connection;
+})(Core || (Core = {}));
+exports.default = Core;
