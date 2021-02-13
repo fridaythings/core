370849ba2a088580a34c5c557cc5a60d5a1321dd
Taras Romilovych <tarasromil@gmail.com>
2021-02-13 19:04:01 +0200

    fix name

--- src/index.ts
+++ src/index.ts
@@ -360,7 +360,7 @@ namespace Core {
     export class PayloadError extends Error {
       toJSON() {
         return {
-          name: PayloadError.constructor.name,
+          name: 'PayloadError',
           message: this.message,
           stack: this.stack,
         };
